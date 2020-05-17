f5c013b1ca8439743557b5f7173b013e583ea317
IgorZmikic <65419153+IgorZmikic@users.noreply.github.com>
2020-05-17 13:24:16 +0200

    Dodao novi tekst

--- WebApp2/WebApp2/Controllers/HomeController.cs
+++ WebApp2/WebApp2/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WebApp2.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page novo dodano Igor.";
 
             return View();
         }
